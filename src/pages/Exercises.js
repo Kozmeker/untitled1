97025158e3d9b616b4f96286f6e9d92e1d3b86f3
Kozmeker <Minecraft-fun-club@email.cz>
2024-04-11 22:01:20 +0200

    admin can make blank new lesson

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import LessonPreview from '../components/exercises/LessonPreview';
 import Modal from '../components/Modal';
 import LessonDetail from '../components/exercises/LessonDetail';
+import AdminLessonDetail from '../components/exercises/AdminLessonDetail';
+import '../assets/styles/pages/exercises.css';
 
 const Exercises = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedLesson, setSelectedLesson] = useState(null);
+    const [isAdmin, setIsAdmin] = useState(true); // Uživatel je administrátor
 
     const openModal = (lesson) => {
         setSelectedLesson(lesson);
@@ -17,6 +20,10 @@ const Exercises = () => {
         setIsModalOpen(false);
     };
 
+    const handleCreateLesson = () => {
+        openModal({ isAdmin: true }); // Otevřít modální okno s AdminLessonDetail
+    };
+
     // Simulace dat o lekcích
     const lessons = [
         { id: 1, title: 'Pilates', date: '2024-04-10', time: '15:00', capacity: 10, registrations: ['Alice', 'Bob', 'Charlie', 'Charlie', 'Charlie', 'Charlie', 'Charlie', 'Charlie', 'Charlie', 'Charlie'] },
@@ -24,7 +31,7 @@ const Exercises = () => {
     ];
 
     return (
-        <div>
+        <div className="container">
             <h1>Cvičení!</h1>
             {/* Zobrazení všech budoucích lekcí */}
             {lessons.map((lesson) => (
@@ -33,8 +40,16 @@ const Exercises = () => {
 
             {/* Modální okno s podrobnostmi o lekci */}
             <Modal isOpen={isModalOpen} onClose={closeModal}>
-                <LessonDetail isAdmin={true} onClose={closeModal} /> {/* Předání prop onClose do LessonDetail */}
+                {/* Podle stavu vybrané lekce zobrazit buď LessonDetail nebo AdminLessonDetail */}
+                {selectedLesson && selectedLesson.isAdmin ? (
+                    <AdminLessonDetail lesson={selectedLesson} onCancel={closeModal} />
+                ) : (
+                    <LessonDetail isAdmin={isAdmin} lesson={selectedLesson} onClose={closeModal} />
+                )}
             </Modal>
+
+            {/* Tlačítko "Vytvořit lekci" pro admina */}
+            {isAdmin && <button className="create-lesson-button" onClick={handleCreateLesson}>Vytvořit lekci</button>}
         </div>
     );
 };
